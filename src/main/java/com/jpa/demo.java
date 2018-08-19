f0d037fdfa932297310c33da7d06cae936f3e524
MartinJwn <martinjwn@163.com>
2018-08-19 14:54:27 +0800

    2、添加了demo

---
+++ src/main/java/com/jpa/demo.java
@@ -0,0 +1,14 @@
+package com.jpa;
+
+/**
+ * @Author huangliujun
+ * @Date 2018-08-19 14:53
+ * @Description
+ */
+public class demo {
+
+    public static void main(String[] args) {
+        System.out.println("args = " + args);
+    }
+
+}
